32b7e85155d8a1c6916fd2bce40b503837f8c3f2
Walisson Aguirra <walissonaguirra@proton.me>
2024-11-06 11:25:17 -0300

    feat: add rule personalizada para Question

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -2,18 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use Closure;
 use App\Models\Question;
 use Illuminate\Http\Request;
+use App\Rules\EndWithQuestionMarkRule;
 
 class QuestionController extends Controller
 {
     public function store()
     {
-        Question::create(
-            request()->validate([
-                'question' => 'required'
-            ])
-        );
+        $attributes = request()->validate([
+            'question' => [
+                'required',
+                'min:10',
+                new EndWithQuestionMarkRule
+            ]
+        ]);
+
+        Question::create($attributes);
 
         return redirect('dashboard');
     }
