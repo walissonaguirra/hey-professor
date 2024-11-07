d4560c67c5dcd107e8d0c344a320d6829120ce89
Walisson Aguirra <walissonaguirra@proton.me>
2024-11-07 10:42:32 -0300

    feat: add butão like e unlike + logica

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,4 +11,19 @@ class Question extends Model
     use HasFactory;
 
     protected $guarded = [];
+
+    public function votes()
+    {
+        return $this->hasMany(Vote::class);
+    }
+
+    public function likes(): Attribute
+    {
+        return new Attribute(fn () => $this->votes()->sum('like'));
+    }
+
+    public function unlikes(): Attribute
+    {
+        return new Attribute(fn () => $this->votes()->sum('unlike'));
+    }
 }
