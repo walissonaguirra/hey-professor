54f3df94ff5dff63ef5fa82a34edba1239eb3b6a
Walisson Aguirra <walissonaguirra@proton.me>
2024-11-07 14:39:53 -0300

    style: removendo form de pergunta da pagina inicial

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -16,29 +16,6 @@
     </div>
 
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 pb-8">
-        <form action="{{ route('question.store') }}" method="post">
-            @csrf
-            <label for="about" class="block text-sm/6 font-medium text-gray-800">Questão</label>
-            <div class="mt-2">
-                <textarea
-                    id="question"
-                    name="question"
-                    rows="3"
-                    class=" block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm
-                            ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2
-                            focus:ring-inset focus:ring-indigo-600 sm:text-sm/6">{{ old('question') }}</textarea>
-            </div>
-
-            @error('question')
-                <p class="mt-3 text-sm/6 text-red-600">{{ $message }}</p>
-            @enderror
-
-            <button type="submit" class="mt-3 rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-                Salva
-            </button>
-        </form>
-
-        <hr class="my-4 border-gray-700 border-dashed"/>
 
         <div class="text-gray-700 uppercase font-bold mb-2">Lista de questões</div>
 
