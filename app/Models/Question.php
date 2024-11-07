a15ba96bad247bcc9302849971d0249d9ae073f3
Walisson Aguirra <walissonaguirra@proton.me>
2024-11-07 10:57:13 -0300

    fix: resolvendo problema N+1 para count dos likes e unlikes

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -15,15 +14,5 @@ class Question extends Model
     public function votes()
     {
         return $this->hasMany(Vote::class);
-    }
-
-    public function likes(): Attribute
-    {
-        return new Attribute(fn () => $this->votes()->sum('like'));
-    }
-
-    public function unlikes(): Attribute
-    {
-        return new Attribute(fn () => $this->votes()->sum('unlike'));
     }
 }
