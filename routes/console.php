231e7007e65efbb1126bccb0b3a64b7751662c3b
Walisson Aguirra <walissonaguirra@proton.me>
2024-11-08 12:21:15 -0300

    feat: delete automaticamente perguntas antiga com mais de um 1 mês

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,12 @@
 <?php
 
 use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\{Artisan, Schedule};
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Schedule::call(function () {
+    Schedule::command('model:prune');
+})->daily();
