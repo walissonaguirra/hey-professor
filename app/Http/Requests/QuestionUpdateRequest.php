db55f4d01c979ab05b31cc413caaab202d9249c9
Walisson Aguirra <walissonaguirra@proton.me>
2024-11-08 08:56:42 -0300

    feat: add form para editar pergunta

--- app/Http/Requests/QuestionUpdateRequest.php
+++ app/Http/Requests/QuestionUpdateRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use App\Rules\EndWithQuestionMarkRule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class QuestionUpdateRequest extends FormRequest
--- resources/views/question/edit.blade.php
+++ resources/views/question/edit.blade.php
@@ -1,3 +1,42 @@
-<div>
-    <!-- Nothing in life is to be feared, it is only to be understood. Now is the time to understand more, so that we may fear less. - Marie Curie -->
-</div>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            Editar pergunta :: {{ $question->id }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    {{ __("You're logged in!") }}
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 pb-8">
+        <form action="{{ route('question.update', $question) }}" method="post">
+            @method('put')
+            @csrf
+            <label for="about" class="block text-sm/6 font-medium text-gray-800">Questão</label>
+            <div class="mt-2">
+                <textarea
+                    id="question"
+                    name="question"
+                    rows="3"
+                    class=" block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm
+                            ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2
+                            focus:ring-inset focus:ring-indigo-600 sm:text-sm/6">{{ old('question', $question->question) }}</textarea>
+            </div>
+
+            @error('question')
+                <p class="mt-3 text-sm/6 text-red-600">{{ $message }}</p>
+            @enderror
+
+            <button type="submit" class="mt-3 rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                Salva
+            </button>
+        </form>
+    </div>
+</x-app-layout>
