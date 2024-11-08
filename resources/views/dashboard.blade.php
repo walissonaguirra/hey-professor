058c53c90829836a302f4572b7c6168fedd154a2
Walisson Aguirra <walissonaguirra@proton.me>
2024-11-08 12:51:41 -0300

    style: add form para pesquisa

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -17,6 +17,32 @@
 
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 pb-8">
 
+        <form action="{{ route('dashboard') }}" method="get">
+            @csrf
+            <label for="about" class="block text-sm/6 font-medium text-gray-800">Pesquisa Questão</label>
+            <div class="mt-2">
+                <input
+                    name="search"
+                    value="{{ request()->input('search') }}"
+                    class=" block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm
+                            ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2
+                            focus:ring-inset focus:ring-indigo-600 sm:text-sm/6"/>
+            </div>
+
+            @error('search')
+                <p class="mt-3 text-sm/6 text-red-600">{{ $message }}</p>
+            @enderror
+
+            <button type="submit" class="mt-3 rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                Buscar
+            </button>
+        </form>
+
+        <br>
+        <br>
+        <br>
+        <br>
+
         <div class="text-gray-700 uppercase font-bold mb-2">Lista de questões</div>
 
         <ul class="text-gray-700">
